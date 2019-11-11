75de78b585cfc1493fcc298a53442d173b36ab57
Alberto <alberto@MacBook-Air-di-Alberto.local>
2019-11-11 20:54:10 +0100

    Updated

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                sassOptions: {
-                    includePaths: ['./node_modules']
-                }
-            }
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: ["./node_modules"]
         }
+      }
     }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/BynaryEncryptor/" : "/"
 };
